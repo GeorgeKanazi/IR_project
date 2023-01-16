591a413f5692e014b063f005c43b35291f612fba
GeorgeKanazi <122729617+GeorgeKanazi@users.noreply.github.com>
2023-01-16 12:44:03 +0200

    Update IR.py

--- IR.py
+++ IR.py
@@ -3,18 +3,20 @@ import pandas as pd
 from inverted_index_gcp import *
 import math
 
-from numpy.linalg import norm # not used
+from numpy.linalg import norm
 from collections import Counter
+
 
 def read_posting_list(inverted, bucket_name, w, folder_name):
     """
     Read posting list of word from bucket store
     :param  inverted: inverted index object
-            name_bucket: name bucket that store the inverted index
-            w: the requested word
+    :param bucket_name : str , name of the bucket
+    :param folder_name : str, name of the folder that contains the indices
+    :param w : the requested word
     :return posting list - list of tuple (doc_id,tf)
     """
-    with closing(MultiFileReader(bucket_name,folder_name)) as reader:
+    with closing(MultiFileReader(bucket_name, folder_name)) as reader:
         locs = inverted.posting_locs[w]
         b = reader.read(locs, inverted.df[w] * 6)
         posting_list = []
@@ -24,13 +26,14 @@ def read_posting_list(inverted, bucket_name, w, folder_name):
             posting_list.append((doc_id, tf))
         return posting_list
 
-        
 
 def get_OPT_Tfidf(q_tokens, index, bucket_name, folder_name, corpus_docs, N=100):
     """
-    Tfidf Optimized function , recieves tokens as a list of tokens (with duplicates)
-    :param q_tokens:  list of tokens , with duplicatse.
+    Tfidf Optimized function , receives tokens as a list of tokens (with duplicates)
+    :param q_tokens:  list of tokens , with duplicates.
     :param index:  inverted index.
+    :param bucket_name : str, name of the bucket
+    :param folder_name : str, name of the folder that contains the indices
     :param corpus_docs : int , optimization - number of docs in corpus
     :param N : int, number of docs to return
     :return: list of documents : title , sorted by their IDF score with query
@@ -41,7 +44,7 @@ def get_OPT_Tfidf(q_tokens, index, bucket_name, folder_name, corpus_docs, N=100)
     for q_word, query_word_count in q_tokens:
         if q_word in index.term_total.keys():
             for doc_id, word_count in read_posting_list(index, bucket_name, q_word, folder_name):
-                if doc_id != 0:  # missing values
+                if doc_id != 0:  # in case of missing values
                     tw = word_count * np.log10(corpus_docs / index.df[q_word])
                     if doc_id in query_sim_dict.keys():
                         query_sim_dict[doc_id] += query_word_count * tw
@@ -53,8 +56,9 @@ def get_OPT_Tfidf(q_tokens, index, bucket_name, folder_name, corpus_docs, N=100)
 
     if len(query_sim_dict) > N:
         return sorted(query_sim_dict, key=query_sim_dict.get, reverse=True)[:N]
-    else:  # return the whole thing
+    else:
         return sorted(query_sim_dict, key=query_sim_dict.get, reverse=True)
+
 
 def calc_BM25(q_tokens, index, bucket_name, folder_name, corpus_docs, avg_dl, k=3, b=0.25):
     q_tokens = list(Counter(q_tokens).items())
@@ -72,20 +76,23 @@ def calc_BM25(q_tokens, index, bucket_name, folder_name, corpus_docs, avg_dl, k=
                             query_sim_dict[doc_id] = tw * q_idf
     return query_sim_dict
 
+
 def get_opt_BM25(q_tokens, index, bucket_name, folder_name, corpus_docs, avg_dl, k=3, b=0.25, N=100):
     """
     BM25 retrieval model from inverted index .
     :param q_tokens - list of str,  tokens of words processed like corpus.
     :param index : inverted index class
+    :param bucket_name : str , name of the bucket
+    :param folder_name : str, name of the folder that contains the indices
     :param corpus_docs : int , optimization - number of docs in corpus
     :param avg_dl : float, average document size in corpus
-    :param k : float, range between [1.2 ,2] optimized to corpus
-    :param b : float , ~0.75 optimizable
+    :param k : float, parameter for the BM25 function
+    :param b : float , parameter for the BM25 function
     :param N : int , number of best-fit docs to retrieve
     :return: list of documents : title , sorted by their IDF score with query
     """
     ####
-    # we found via optmization that the optimal parameter are:
+    # we found via optimization that the optimal parameter are:
     # k = 3
     # b = 0.25
     # we set those values as a default
@@ -98,38 +105,38 @@ def get_opt_BM25(q_tokens, index, bucket_name, folder_name, corpus_docs, avg_dl,
         return sorted(query_sim_dict, key=query_sim_dict.get, reverse=True)
 
 
-
-    
-
-
 def get_opt_BM25_for_joint(q_tokens, index, corpus_docs, avg_dl, bucket_name, folder_name, k=3, b=0.25, N=100):
     """
-    BM25 retrieval model from inverted index , intended for joining indices (returns tf score as well as doc)
+    BM25 retrieval model from inverted index , intended for joining indices
     :param q_tokens - list of str,  tokens of words processed like corpus.
     :param index : inverted index class
     :param corpus_docs : int , optimization - number of docs in corpus
     :param avg_dl : float, average document size in corpus
-    :param k : float, range between [1.2 ,2] optimized to corpus
-    :param b : float , ~0.75 optimizable
+    :param bucket_name : str , name of the bucket
+    :param folder_name : str, name of the folder that contains the indices
+    :param k : float, parameter for the BM25 function
+    :param b : float , parameter for the BM25 function
     :param N : int , number of best-fit docs to retrieve
-    :return: list of tokens : (title,tf-idf score) , sorted by their IDF score with query
+    :return: list of tokens : (title,BM25 score) , sorted by their BM25 score with query
     """
     
     query_sim_dict = calc_BM25(q_tokens, index, bucket_name, folder_name, corpus_docs, avg_dl)
 
     if len(query_sim_dict) > N:
         return [(key, query_sim_dict[key]) for key in sorted(query_sim_dict, key=query_sim_dict.get, reverse=True)[:N]]
-    else:  # return the whole thing
+    else:
         return [(key, query_sim_dict[key]) for key in sorted(query_sim_dict, key=query_sim_dict.get, reverse=True)]
+
 
 def get_OPT_Cosine(q_tokens, index, bucket_name, folder_name, N=100):
     """
-    Tfidf Optimized function , recieves tokens as a list of tokens (with duplicates)
-    :param q_tokens:  list of tokens , with duplicatse.
+    Cosine Similarity on TF-IDF scores.
+    :param q_tokens:  list of tokens , with duplicates.
     :param index:  inverted index.
-    :param corpus_docs : int , optimization - number of docs in corpus
+    :param bucket_name : str , name of the bucket
+    :param folder_name : str, name of the folder that contains the indices
     :param N : int, number of docs to return
-    :return: list of documents : title , sorted by their IDF score with query
+    :return: list of documents : title , sorted by their cosine similarity score with query
     """
     query_size = len(q_tokens)
     query_counter = Counter(q_tokens)
